fa528d2f6409e8d5c51cae1665ec95a5c81dba2c
devi213 <38770398+devi213@users.noreply.github.com>
2024-05-17 19:16:12 -0700

    Update PlayerController.cs
    Fixed players breaking instantly

--- Assets/Scripts/Overworld/PlayerController.cs
+++ Assets/Scripts/Overworld/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
-    private Tilemap tilemap;
-
     private Rigidbody2D Rigidbody2D;
 
     public float Speed;
+
+    void Start()
+    {
+        Rigidbody2D = transform.GetComponent<Rigidbody2D>();
+    }
+
     private void FixedUpdate()
     {
         Vector3 MoveVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
